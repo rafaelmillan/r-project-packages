a8f6506cd14957039733f92872d416d4316509b2
Rafael Millan <rafmillan@gmail.com>
2018-12-04 08:21:30 +0100

    Redirect root to packages index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: redirect('/packages')
+
   resources :packages, only: [:index] do
     resources :versions, only: [:show]
   end
